f5e3e9687b298c8c89334f5e0073535c794fe10d
yunhozz <qkrdbsgh1121@naver.com>
2023-03-07 20:07:58 +0900

    feat: createBoard 메소드 생성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid'; // npm install uuid --save 명령어를 이용하여 설치 -> UUID import
 
 @Injectable()
 export class BoardsService {
-  private boards = []; // local memory
+  private boards: Board[] = []; // local memory
 
-  findAllBoards() {
+  findAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string): Board {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
