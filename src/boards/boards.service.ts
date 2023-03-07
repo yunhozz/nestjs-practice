2a261fe1edc5df9703e32a35683900d594f0e5fe
yunhozz <qkrdbsgh1121@naver.com>
2023-03-07 22:43:21 +0900

    comment: 파이프에 대한 설명 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -51,3 +51,21 @@ export class BoardsService {
     this.boards = this.boards.filter((board) => board.id !== id);
   }
 }
+
+/*
+<PIPE 란?>
+1. @Injectable() 데코레이터로 주석이 달린 클래스
+2. data transformation 과 data validation 을 위해 사용
+3. 컨트롤러 경로 처리기에 의해 처리되는 인수에 대해 작동
+
+data transformation: 입력 데이터를 원하는 형식으로 변환 ex) String to Integer
+data validation: 입력 데이터를 평가하고 유효하지 않은 경우 예외를 발생시킴
+
+<PIPE 를 사용하는 방법>
+1. Handler-level Pipes: @UsePipes() 데코레이터를 이용해서 사용, 모든 파라미터에 적용
+2. Parameter-level Pipes: 특정한 파라미터에만 적용
+3. Global-level Pipes: 애플리케이션 레벨의 파이프, 클라이언트에서 들어오는 모든 요청에 적용, main.ts 에 적용
+
+<기본적으로 사용할 수 있는 PIPE>
+ValidationPipe, ParseIntPipe, ParseBoolPipe, ParseArrayPipe, ParseUUIDPipe, DefaultValuePipe
+ */
