cbe56483dcf910d6ba00f8b2b10fb122b26c6766
yunhozz <qkrdbsgh1121@naver.com>
2023-03-09 20:13:49 +0900

    feat: board 에 대한 CRUD 생성

---
+++ src/boards/controller/boards.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { BoardsService } from '../service/boards.service';
+import { Board } from '../board.entity';
+import { CreateBoardDto } from '../dto/create-board.dto';
+import { BoardStatus } from '../board-status.enum';
+import { BoardStatusValidationPipe } from '../pipes/board-status-validation.pipe';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardService: BoardsService) {}
+
+  @Get('/:id')
+  getBoardById(@Param('id', ParseIntPipe) id: number): Promise<Board> {
+    return this.boardService.findBoardById(id);
+  }
+
+  @Get()
+  getAllBoards(): Promise<Board[]> {
+    return this.boardService.findAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Patch('/:id')
+  updateBoardStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ): Promise<Board> {
+    return this.boardService.updateBoardStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteBoardById(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.boardService.deleteBoardById(id);
+  }
+}
---
+++ src/boards/service/boards.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardRepository } from '../repo/board.repository';
+import { Board } from '../board.entity';
+import { CreateBoardDto } from '../dto/create-board.dto';
+import { BoardStatus } from '../board-status.enum';
+
+@Injectable()
+export class BoardsService {
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {} // DI repository
+
+  async findBoardById(id: number): Promise<Board> {
+    const board = await this.boardRepository.findOneBy({ id });
+
+    if (!board) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return board;
+  }
+
+  async findAllBoards(): Promise<Board[]> {
+    return await this.boardRepository.find();
+  }
+
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return await this.boardRepository.createBoard(createBoardDto);
+  }
+
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.findBoardById(id);
+    board.status = status;
+    return await this.boardRepository.save(board);
+  }
+
+  async deleteBoardById(id: number): Promise<void> {
+    const result = await this.boardRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    console.log('result', result); // id 로 조회 실패시 'affected = 0' -> 예외 처리
+  }
+}
+
+/*
+동기: 특정한 작업이 종료된 후 다음 줄의 작업을 실행하는 것을 의미 (직렬)
+비동기: 특정한 작업이 종료되기 까지 기다리는 것이 아니라 기다리지 않고 실행되는 것 (병렬)
+
+js 에서는 대부분의 작업들이 특별한 처리를 해주지 않으면 자동적으로 비동기적으로 실행되는 부분들이 많다.
+따라서, Promise 와 async/await 구문을 사용하여 처리한다.
+ */
