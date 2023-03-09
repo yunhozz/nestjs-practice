87f9c5b384aa97321c72a007a89ee95aff64bbb0
yunhozz <qkrdbsgh1121@naver.com>
2023-03-10 07:48:38 +0900

    chore: jwt 관련 설정 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { CustomOrmModule } from '../config/custom-orm.module';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), CustomOrmModule.forCustomRepository([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    CustomOrmModule.forCustomRepository([UserRepository]),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: { expiresIn: 60 * 60 },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
